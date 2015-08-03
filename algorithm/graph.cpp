db4c378d166f349b62b4e3dcbf70c6198c820e48
n-kats <n-kats19890214@hotmail.co.jp>
2015-08-04 05:06:43 +0900

    fix bug

--- algorithm/graph.cpp
+++ algorithm/graph.cpp
@@ -1,19 +1,25 @@
 #include <vector>
+#include <iostream>
+class GraphNode;
+class TreeNode;
+
 class GraphNode{
   std::vector<GraphNode* > neighbors;
 };
 
-TreeNode{
+class TreeNode{
   TreeNode* parent;
   TreeNode* root(){
     TreeNode* pt = this;
-    while(*pt.parent != nullptr){
-      pt = *pt.parent;
+    while((*pt).parent != nullptr){
+      pt = (*pt).parent;
     }
     return pt;
   }
 };
 
+using namespace std;
 int main(){
-
+	cout << "hoge" << endl;
+	return 0;
 }
