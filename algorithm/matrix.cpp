41f3d8f9e77f6c39a7f13834ea914521ab3b217b
Katsunori Nakanishi <n-ksts19890214@hotmail.co.jp>
2015-08-06 17:53:08 +0900

    prepare basic classes

--- algorithm/matrix.cpp
+++ algorithm/matrix.cpp
@@ -6,20 +6,56 @@
 
 using namespace std;
 
-using Matrix vector<vector<int> >
+
+class Tensor{
+	vector<int> sizes;
+	vector<int> values;
+};
+
+class Matrix{
+public:
+	int height;
+	int width;
+private:
+	vector<int> values;
+public:
+	Matrix(){
+		height = 0;
+		width = 0;
+	}
+
+	Matrix(int h, int w, vector<int> vals){
+		if(h > 0 && w > 0 && vals.size() == h * w){
+			height = h;
+			width = w;
+			values = vals;
+		}else{
+			height = 0;
+			width = 0;
+			values.clear();
+			// cerr << illegal input << endl;
+		}
+	}
+};
+
+class Formula{
+};
+
 class LinearAlgebra{
+public:
 	LinearAlgebra(){}
-	
+
+
 	Matrix load(string s){
-		
+		return Matrix();
 	}
 	int rank(Matrix m){
-	
+		return 0;
 	}
-	
+
 };
 
 int main(){
-	LinearAlgebraTheory la;
+	LinearAlgebra la;
 	return 0;
 }
